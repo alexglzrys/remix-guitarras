b507580137b5d7345d13c846996c0a19f1ab3c97
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-25 14:13:18 -0600

    agregar metainformación y hojas de estilo

--- app/root.jsx
+++ app/root.jsx
@@ -6,16 +6,27 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from './styles/index.css'
 
+// metainformación a cargar en el header de la aplicación
+// Se requiere importar el componente Meta para que inyecte automáticamente esta información
 export const meta = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "GuitarLA - Remix",
   viewport: "width=device-width,initial-scale=1",
 });
+// hojas de estilo a cargar en el header de la aplicación
+// Se requiere el componente Links para que inyecte automáticamente esta información
+export const links = () => ([
+  {
+    rel: 'stylesheet',
+    href: styles
+  }
+])
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="es-MX">
       <head>
         <Meta />
         <Links />
