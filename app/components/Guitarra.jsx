ce16ddeaff0f012a4b6990dd972388571e48d7f1
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-26 16:36:24 -0600

    estructura y diseño - página de tienda

--- app/components/Guitarra.jsx
+++ app/components/Guitarra.jsx
@@ -1,7 +1,23 @@
-const Guitarra = ({guitarra}) => {
-  return (
-    <div>Guitarra</div>
-  )
-}
+import { Link } from "@remix-run/react";
 
-export default Guitarra
+const Guitarra = ({ guitarra }) => {
+  const { titulo, precio, descripcion, imagen, url } = guitarra;
+  return (
+    <div className="guitarra">
+      <img
+        src={imagen.data.attributes.formats.medium.url}
+        alt={`Imagen de guitarra ${titulo}`}
+      />
+      <div className="contenido">
+        <h3 className="titulo">{titulo}</h3>
+        <p className="descripcion">{descripcion}</p>
+        <p className="precio">$ {precio}</p>
+        <Link className="enlace" to={`guitarras/${url}`}>
+          Ver producto
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Guitarra;
--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "@remix-run/react";
 import Guitarra from "../components/Guitarra";
 import { getGuitarras } from "../models/guitarras.server"
+import css from '../styles/guitarras.css'
 
 // Consumir datos
 // (mismo enfoque que el API de React Router Dom 6.4 y sus loaders)
@@ -9,14 +10,25 @@ export async function loader() {
   return guitarras.data;
 } 
 
+// Metainformación para esta página
 export const meta = () => ({
-    title: 'GuitarLA - Tienda'
+    title: 'GuitarLA - Tienda de Guitarras',
+    description: 'Venta de instrumentos de música'
 })
+
+// links o referencias de archivos para esta página
+// Los estilos css se heredan a los componentes internos de esta página
+export const links = () => ([
+  {
+    rel: 'stylesheet',
+    href: css
+  }
+])
 
 const Tienda = () => {
   // Recuperar el resultado de la petición http (loadoer)
   const guitarras = useLoaderData();
-  console.log(guitarras)
+ 
   return (
     <div className="contenedor">
         <h2 className="heading">Nuestra Colección</h2>
