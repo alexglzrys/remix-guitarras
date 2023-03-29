b8df2a4200f41176a01829a5f64efcec0733bf2f
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-28 18:36:14 -0600

    formulario para agregar al carrito de compras

--- app/routes/guitarras/$guitarraSlug.jsx
+++ app/routes/guitarras/$guitarraSlug.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from '@remix-run/react';
-import React from 'react'
+import React, { useState } from 'react'
 import { getGuitarra } from '../../models/guitarras.server';
 import css from '../../styles/guitarras.css'
 
@@ -47,8 +47,43 @@ export const meta = ({data}) => {
 }
 
 const GuitarraPage = () => {
+    /**
+     * Remix
+     * Es un framework que trabaja tanto en el servidor como en el cliente
+     * Por eso es importante saber distinguir las API's a utilizar, ya que algunas solo están disponibles solo en el servidor
+     * o en el cliente, como LocalStorage.
+     * 
+     * Lo mismo pasa con los hooks de React, estos solo están disponobles para el cliente, es decir los componentes
+     * Por tanto no se pueden usar dentro de la API de Remix (loaders, links, meta)
+     */
+
+    // Estado local del formulario de carrito de compras
+    const [cantidad, setCantidad] = useState(0)
+
     const guitarra = useLoaderData();
     const {titulo, descripcion, imagen, precio,} = guitarra.data[0].attributes;
+
+    // Controlador de agregar al carrito
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        // Validar cantidad seleccionada
+        if (cantidad < 1) {
+            alert('Debe seleccionar una cantidad de producto válida');
+            return false
+        }
+
+        // Generar objeto que representa al producto en el carrito de compra
+        const producto = {
+            id: guitarra.data[0].id,
+            titulo,
+            precio,
+            cantidad,
+            imagen: imagen.data.attributes.url
+        }
+        console.log(producto)
+    }
   return (
     <div className='guitarra'>
         <img src={imagen.data.attributes.url} alt={`Imagen de guitarra ${titulo}`} />
@@ -56,6 +91,20 @@ const GuitarraPage = () => {
             <h3>{titulo}</h3>
             <p className="texto">{descripcion}</p>
             <div className="precio">$ {precio}</div>
+
+            <form onSubmit={handleSubmit} className="formulario">
+                <label htmlFor="cantidad">Cantidad</label>
+                <select id="cantidad" onChange={e => setCantidad(parseFloat(e.target.value))}>
+                    <option value="0">-- Seleccione --</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <input type="submit" value="Agregar al carrito" />
+            </form>
+
         </div>
     </div>
   )
